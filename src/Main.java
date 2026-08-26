986186d2fcc769b8feab3f567e70911615a303f3
namkungkang <namkung0131@naver.com>
2026-08-26 14:33:01 +0900

    변수, 상수

---
+++ src/Main.java
@@ -0,0 +1,42 @@
+//TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
+// 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
+public class Main {
+    public static void main(String[] args) {
+        int count = 10;
+
+        count = 20;
+
+        System.out.println(count);
+
+        double avg;
+        avg = 11.1;
+
+        System.out.println(avg);
+
+        String name = "qqq";
+
+        int totalConut;
+
+        totalConut = 1000;
+
+        System.out.println(name);
+        System.out.println(totalConut);
+
+
+        // 원의 넓이 구하기
+        double circleArea;
+
+        final double PI = 3.14;
+
+        circleArea = 3 * 3 * PI;
+
+        System.out.println(circleArea);
+
+        //기름값 표시
+        final int OIL_PRICE = 1450;
+        System.out.println(OIL_PRICE);
+
+        int totalPrice = 50 * OIL_PRICE;
+        System.out.println(totalPrice);
+    }
+}
