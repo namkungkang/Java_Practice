5491f14384989c18ae5c30a69c826715cdb1f793
namkungkang <namkung0131@naver.com>
2026-08-27 11:29:14 +0900

    조건,삼항 연산자

--- src/Main.java
+++ src/Main.java
@@ -2,28 +2,22 @@
 // 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
 public class Main {
     public static void main(String[] args) {
-        //if문
-        int x = 50;
-        int y = 50;
-        if(x > y) {
-                System.out.println("x는 y보다 작습니다.");
-        }
-        //중괄호 없는 구문
-        if(x > y)
-            System.out.println("x와 y는 같다.");
-        System.out.println("test");
+        //논리 연산자
+       boolean a = true;
+       boolean b = false;
+       boolean c = true;
 
-        //else if, else 추가문
-        if (x > y) {
-            System.out.println("같다");
-        } else if (x < y) {
-            System.out.println("아니유");
-            
-        }else  {
-            System.out.println("다르다");
-        }
+        System.out.println(a == b);
+        System.out.println(a || b);
+        System.out.println(a && c);
+        System.out.println(b || c);
+        System.out.println(a || c);
+        System.out.println(b || b);
+        System.out.println(a ^ c);
 
-
+        //삼항 연산자
+        int b1 = (5<4) ? 20 : 30;
+        System.out.println(b1);
     }
 
 
