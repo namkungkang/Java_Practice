571f72909bf98ce99351c8c5ee6d58d412933381
namkungkang <namkung0131@naver.com>
2026-08-28 15:06:23 +0900

    Array문

--- src/Main.java
+++ src/Main.java
@@ -4,22 +4,47 @@ import java.util.Scanner;
 // 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
 public class Main {
     public static void main(String[] args) {
-        //for문
-        int total = 0;
-        for (int i = 1; i<=100; i++) {
-            total = total + i;
-        }
-        System.out.println(total);
-        int total2 = 0;
-            for(int y=0; y<=100; y++) {
-                if(y % 2 == 0) {
-                    if (y==50) {
-                        break;
-                    }
-                total2 = total2 + y;
-            }
-        }
-        System.out.println(total2);
+     int [] array1 = new int[10];
+     array1[1] = 10;
+     array1[9] =20;
+     System.out.println(array1.length);
+
+     int[] array3 = {1,2,3,4};
+
+     System.out.println(array3[3]);
+
+     System.out.println(array1[9]);
+
+     int[] array4 = new int[100];
+     array4[0] = 1;
+     array4[1] = 2;
+
+     int[] array5 = new int[10];
+
+     for (int i =0;i<10; i++){
+         array5[i] = i;
+     }
+
+     int[] iarray = new int[10];
+     int sum = 0;
+
+     for(int i=0; i<iarray.length; i++) {
+         iarray[i] = i;
+        sum = sum + iarray[i];
+
+     }
+        System.out.println(sum);
+
+
+        //2차원 배열
+        int [] [] array9 = new int[3][4];
+        array9[0][0] = 10;
+        array9[1][2] = 101;
+
+        System.out.println(array9[1][2]);
 
     }
+;
+
+
 }
