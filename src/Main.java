94d400691d239a1bb1bbd09f169a0ab075ff6a57
namkungkang <namkung0131@naver.com>
2026-08-31 11:31:10 +0900

    Class문

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,53 @@ import java.util.Scanner;
 // 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
 public class Main {
     public static void main(String[] args) {
+//        CarExam c1 = new CarExam();
+//
+//        String str = new String("hello");
+//        String str2 = "hello";
+//        String str3 = new String("hello");
+//        String str4 = "hello";
+//
+//        if (str == str2) {
+//            System.out.println("O");
+//        }
+//
+//        if (str == str3) {
+//            System.out.println("O");
+//        }
+//        else System.out.println("X");
+//
+//        if (str2 == str4) {
+//            System.out.println("O");
+//        }
+//
+//        System.out.println(str3.substring(3));
+//        System.out.println(str3);
+//
+//        System.out.println(str2);
+//        System.out.println(str);
 
+        String str = "hi";
+        String str2 = new String("hi");
+        String str3 = "hi";
 
+        if (str == str3) {
+            System.out.println("맞아요");
+        }
 
+        if (str2 == str3) {
+            System.out.println("맞나여?");
+        }
+
+        if (str.equals(str3)) {
+            System.out.println("맞음");
+        }
+
+        if (str.equals(str2)) {
+            System.out.println("맞아");
+        }
 
         }
+
+
     }
