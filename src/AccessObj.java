705053e76f67bdc1e766ec1e3ca0079fb0a5131d
namkungkang <namkung0131@naver.com>
2026-09-02 14:30:35 +0900

    AccessObj

---
+++ src/AccessObj.java
@@ -0,0 +1,14 @@
+
+
+public class AccessObj {
+    public int  p = 3;
+    protected int p2 = 10;
+    private int p3 = 20;
+    int k = 30; //default 접근지정자 같은 패키지내에서 접근 가능
+
+
+
+    //public >> protected >> private
+
+
+}
---
+++ src/AccessObjExam.java
@@ -0,0 +1,16 @@
+public class AccessObjExam  {
+    public static void main (String[] args) {
+        AccessObj accessObj = new AccessObj();
+
+        System.out.println(accessObj.p);
+        System.out.println(accessObj.p2);
+        System.out.println(accessObj.k);
+//        System.out.println(accessObj.p3);
+
+
+
+    }
+
+
+
+}
