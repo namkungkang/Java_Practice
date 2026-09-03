51b8adba11b65f4fccde06d31ddf528594d3cc3d
namkungkang <namkung0131@naver.com>
2026-09-03 14:15:30 +0900

    TypeCasting

--- src/Bus.java
+++ src/Bus.java
@@ -2,14 +2,10 @@
 
 
 public class Bus extends Car1 {
-
-    public void bbang() {
+    public void BBang() {
         System.out.println("빵빵");
     }
 
-    public void Run() {
-        super.Run(); //부모의 메서도 호출 가능 오버라이딩시
-        System.out.println("안달려");
-    }
+
 
 }
--- src/BusExam.java
+++ src/BusExam.java
@@ -1,13 +1,21 @@
 public class BusExam {
     public static void main(String[] args) {
-    Bus bus = new Bus();
+//    Bus bus = new Bus();
+
+
+    Car1 c= new Bus();
+    c.Run();
+
+    Bus bus = (Bus) c ;
+    bus.Run();
+    bus.BBang();
 //    bus.run();
-    bus.bbang();
-    bus.Run();
-
-
-    Car1 car1 = new Bus();
-    bus.Run();
+//    bus.bbang();
+//    bus.Run();
+//
+//
+//    Car1 car1 = new Bus();
+//    bus.Run();
 
 
 //    Car1 car = new Car1();
