86822de0169aac10d851ac59b665cdbe092a2be2
namkungkang <namkung0131@naver.com>
2026-09-03 11:28:07 +0900

    Abstract

---
+++ src/DuckExam.java
@@ -0,0 +1,13 @@
+public class DuckExam {
+    public static void main(String[] args) {
+
+
+        Duck duck = new Duck();
+
+        duck.sing();
+        duck.fly();
+
+//        Bird bird = new Bird();
+    }
+
+}
